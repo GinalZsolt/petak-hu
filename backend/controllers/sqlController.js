464f99e9069bfa67f83f4f8ae7949697ad09f526
DavidKocsis0204 <kocsisdavid20020204@gmail.com>
2023-01-17 09:09:56 +0100

    select one, select all, delete one, delete all kész

--- backend/controllers/sqlController.js
+++ backend/controllers/sqlController.js
@@ -10,3 +10,34 @@ Router.get('/:table', (req, res)=>{
         else res.status(200).send(result);
     })
 })
+
+//select one sql
+Router.get('/:table/:field/:value', (req, res)=>{
+    let table_name=req.params.table;
+    let table_field=req.params.field;
+    let field_value=req.params.value;
+    config.pool.query(`SELECT * FROM ? WHERE ${field}=${value}`, [table_name, table_field, field_value], (err, result)=>{
+        if(err) res.status(500).send(err);
+        else res.status(200).send(result);
+    })
+})
+
+//delete one sql
+Router.delete('/:table/:field/:value', (req, res)=>{
+    let table_name=req.params.table;
+    let table_field=req.params.field;
+    let field_value=req.params.value;
+    config.pool.query(`DELETE FROM ? WHERE ${field}=${value}`, [table_name, table_field, field_value], (err, result)=>{
+        if(err) res.status(500).send(err);
+        else res.status(200).send(result);
+    })
+})
+
+//delete all sql
+Router.delete('/:table', (req, res)=>{
+    let table_name=req.params.table;
+    config.pool.query(`DELETE FROM ?`, [table_name], (err, result)=>{
+        if(err) res.status(500).send(err);
+        else res.status(200).send(result);
+    })
+})
