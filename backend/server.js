de675dfe2c3343d1087fe38c50003b82eef3bda4
zickbalazs <zickbalazs@turr.hu>
2023-01-17 08:29:53 +0100

    Alap szerver kész

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+  
+});
