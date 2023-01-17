9f442127acd54d15e14b486fb112933191af719c
DavidKocsis0204 <kocsisdavid20020204@gmail.com>
2023-01-17 08:56:54 +0100

    sql elkezdve

--- backend/controllers/sqlController.js
+++ backend/controllers/sqlController.js
@@ -1,2 +1,9 @@
 const Router = require('express').Router();
+var mysql = require('mysql');
+var pool = mysql.createPool();
 
+//select all sql
+Router.get('/:table', (req, res)=>{
+    let table_name=req.params.table;
+    pool.
+})
