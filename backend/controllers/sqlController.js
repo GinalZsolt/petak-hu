b01e72adebafdf07778fa2d0fa58b4bee8593103
zickbalazs <zickbalazs@turr.hu>
2023-01-17 11:02:36 +0100

    Update

--- backend/controllers/sqlController.js
+++ backend/controllers/sqlController.js
@@ -1,3 +1,19 @@
 const Router = require('express').Router();
-const mysql = require('mysql');
+const pool = require('../config').pool;
 
+Router.patch('/:tablename/:id', (req,res)=>{
+    let update = "";
+    let keys = Object.values(req.body);
+    let values = Object.values(req.body);
+    for (let i = 0; i < keys.length; i++) {
+        update +=`${keys[i]}='${values[i]}'`;
+    }
+    pool.query(`update ${req.params.tablename} set ${update} where ID=${req.params.id}`, (err,data)=>{
+        if (err) res.status(500).send(err.message);
+        else res.status(200).send(data);
+    });
+})
+
+
+
+module.exports = Router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const http = require('http');
 const cors = require('cors');
 const server = http.createServer(app);
 
+app.use('/api', require('./controllers/sqlController'))
+
+
 server.listen(process.env.PORT, () => {
-  
+  console.log('http://localhost:8080')
 });
