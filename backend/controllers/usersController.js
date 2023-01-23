7b2f905f47f5349b9f073ed88fbecef12b7fd9b8
zickbalazs <zickbalazs@turr.hu>
2023-01-23 09:37:18 +0100

    Felhasználókezelés backend részről kész

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -7,19 +7,33 @@ Router.post('/login', (req,res)=>{
             message: 'Rossz bejelentkezési adatok!',
             type:'danger'
         });
-        pool.query('select * from moderations where userID=?', [data[0].ID], (err, results)=>{
-            if (err) res.status(500).send(err.message);
-            else {
-                if (results.length==0) res.status(200).json({
-                    message:'Sikeres login!',
-                    userData: data[0]
-                });
-                else res.status(403).json({
-                    message:'Tiltott felhasználó!',
-                    type:'danger'
-                })
-            }
-        })
+        else{
+            pool.query('select * from moderations where userID=?', [data[0].ID], (err, results)=>{
+                if (err) res.status(500).send(err.message);
+                else {
+                    if (results.length==0) res.status(200).json({
+                        message:'Sikeres login!',
+                        userData: data[0]
+                    });
+                    else res.status(403).json({
+                        message:'Tiltott felhasználó!',
+                        type:'danger'
+                    })
+                }
+            })
+        }
+    })
+})
+Router.post('/register', (req,res)=>{
+    pool.query(`insert into users (${Object.keys(req.body).join(', ')}) values (${Object.values(req.body).map(e=>"'"+e+"'").join(', ')})`, (err, data)=>{
+        if (err){
+            if (err.code==="ER_DUP_ENTRY") res.status(400).send({
+                message: 'Használt e-mail cím!',
+                type:'danger'
+            })
+            else{res.status(500).send(err);}
+        }
+        else res.status(200).send(data);
     })
 })
 
