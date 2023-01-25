5ce4fea9a70ca4b98c5950f0e4392432d8511d26
zickbalazs <zickbalazs@turr.hu>
2023-01-25 12:29:36 +0100

    Aukciókártya elkezdve

--- frontend/src/components/subcomponents/AuctionCard.svelte
+++ frontend/src/components/subcomponents/AuctionCard.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   import type { Auction } from "../../classes/Auction";
-  export let Auction:Auction;
+  export let Auction: Auction;
 </script>
 
 <div>
   <div class="auctimg">
-    <img src={Auction.coin.images[0]} alt="{Auction.title}" />
+    <img src={Auction.coin.images[0]} alt={Auction.title} />
   </div>
-  <div class="bottomText">
+  <div class="bottomText d-flex justify-content-between">
     <p>{Auction.coin.name}</p>
     <p>{Auction.price} Ft</p>
   </div>
 </div>
+
 <style lang="sass">
+
 </style>
