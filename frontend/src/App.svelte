07770fb10d032ec63ec4a1fa7298c69f4eeabd0e
ginalzsolt <ginalzsolt@turr.hu>
2023-01-31 09:17:28 +0100

    post-lg-md grafikus terv módosítás

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -7,4 +7,4 @@
 </script>
 
 
-<Login/>
+<BlogPost Data={{author:"mintapista",date:"2023-01-31",title:"Tesztcím a blogposthoz",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque libero iusto magnam odio at eaque vitae reiciendis, iste quia voluptatem maiores aliquid nesciunt voluptate non maxime alias minus. Odit, optio."}}/>
--- frontend/src/components/BlogPost.svelte
+++ frontend/src/components/BlogPost.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+    import { dataset_dev } from "svelte/internal";
     import Comment from "./subcomponents/Comment.svelte";
-
+    export let Data:any={}
 </script>
 <style lang="sass">
     #newcomment
@@ -15,12 +16,22 @@
         border:1px solid black
         background-color: #ea9e60
         border-radius: 0 0.25rem 0.25rem 0
+    #post
+        border:1px solid black
+        border-radius:0.25rem
+        background-color: #ffcc95
+        padding:10px
 </style>
 <!-- Content -->
 
 <main>
-    <div id="post">
-
+    <div id="post" class="col-lg-8 col-md-8 col-11 mx-auto ">
+        <h2>{Data.title}</h2>
+        <div id="authorinfo">
+            <span>{Data.author}</span>
+            <span>{Data.date}</span>
+        </div>
+        <p>{Data.text}</p>
     </div>
     <div id="newcomment" class=" col-lg-8 col-md-8 col-11 mx-auto mt-3">
         <div class="input-group ">
