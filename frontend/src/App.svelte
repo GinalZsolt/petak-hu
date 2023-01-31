e31f981f6a4887c36d8c0d64be3fa2fe2d8ad04d
zickbalazs <zickbalazs@turr.hu>
2023-01-31 09:39:58 +0100

    Aukciós kártya, oldal, licitálók kész

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,9 +2,26 @@
 
 </style>
 <script lang="ts">
-    import Login from "./components/Login.svelte";
-    import BlogPost from "./components/BlogPost.svelte";
+  import { Auction } from "./classes/Auction";
+  import { Coin } from "./classes/Coin";
+    import AuctionPage from "./components/AuctionPage.svelte";
+    let testAuction: Auction = new Auction({
+    ID: 0,
+    price: 11,
+    title: "Lorem",
+    minBid: 50,
+    description: "Lorem ipsum dolor sit amet",
+    expiration: new Date(),
+  });
+  testAuction.coin = new Coin({
+    ID: 0,
+    worth: 11,
+    name: "Zseton",
+    description: "Zseton leírás",
+    headfile: "test.png",
+    tailfile: "test.png",
+  });
 </script>
 
 
-<Login/>
+<AuctionPage auction={testAuction} />
