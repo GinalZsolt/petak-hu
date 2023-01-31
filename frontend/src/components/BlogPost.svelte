8e43cc117165fe0f8f93d17dbed99590b99cb760
ginalzsolt <ginalzsolt@turr.hu>
2023-01-31 08:43:43 +0100

    comment input done

--- frontend/src/components/BlogPost.svelte
+++ frontend/src/components/BlogPost.svelte
@@ -9,11 +9,9 @@
         background-color: #ffcc95
         padding:10px
     #newcomment input
-        width: 95%
         border:1px solid black
         border-radius: 0.25rem 0 0 0.25rem 
     #newcomment button
-        width:5%
         border:1px solid black
         background-color: #ea9e60
         border-radius: 0 0.25rem 0.25rem 0
@@ -24,8 +22,11 @@
     <div id="post">
 
     </div>
-    <div id="newcomment" class=" col-lg-8 col-md-8 col-11 mx-auto">
-        <input type="text" name="message" id="message"><button type="button"><i class="bi bi-caret-right-square"></i></button>
+    <div id="newcomment" class=" col-lg-8 col-md-8 col-11 mx-auto mt-3">
+        <div class="input-group ">
+            <input type="text" name="message" id="message" class="form-control">
+            <button type="button" class="btn bi bi-send input-group-text"></button>
+        </div>
     </div> 
     <Comment Data={{username:"Béla",date:"2023-01-30",text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem vel sint sequi sit voluptate culpa id, similique natus minus odit, minima doloremque tenetur nostrum at eos! Necessitatibus explicabo aperiam illum."}}/>
 </main>
