7605a17de0eb81eff2893549d47f47398a3ccfe6
zickbalazs <zickbalazs@turr.hu>
2023-02-01 13:21:15 +0100

    Aukciós oldal a grafikus tervnek megfelelően kész

--- frontend/src/components/AuctionPage.svelte
+++ frontend/src/components/AuctionPage.svelte
@@ -12,20 +12,23 @@
       <h2 class="mb-0">{username} - {auction.title}</h2>
     </aside>
     <div class="row mx-auto">
-      <div class="ps-md-0 col-md-6 col-12">
+      <div class="ps-md-0 col-md-6 col-12 mb-md-0 mb-3">
         <div class="w-100 h-100" id="AuctionCoin">
           <img class="img-fluid h-100" src={auction.coin.images.head} alt="" />
-          <p class="w-100" id="title">{auction.coin.name}</p>
+          <p class="w-100 px-2" id="title">{auction.coin.name}</p>
         </div>
       </div>
       <div class="col-md-6 col-12 pe-md-0">
-        <div class="row">
-          <div class="input-group mb-3">
-            <input type="number" name="bidAmount" class="form-control border-dark" />
+        <h3>Licitálás</h3>
+        <div class="d-flex flex-row mb-3">
+          <div class="input-group me-lg-3">
+            <input type="number" min={auction.price +auction.minBid} value={auction.price + auction.minBid} name="bidAmount" class="form-control border-dark" />
             <label for="bidAmount" class="input-group-text border-dark">Ft</label>
           </div>
+          <button class="btn border-dark">Licitálás</button>
         </div>
         <p class="border-bottom border-dark pb-3">Licitlépcső: {auction.minBid} Ft</p>
+        <h3>Legutóbbi licitek</h3>
         <div id="Bidders">
           <p>mintajanos02 - 200 Ft {new Date().toISOString()}</p>
           <p>mintajanos02 - 200 Ft {new Date().toISOString()}</p>
@@ -59,6 +62,8 @@ hr
   background-color: black
   position: relative
   overflow: hidden
+  img
+    border: 1px solid black
 .input-group-text
   background-color: #e99d60
 #title
