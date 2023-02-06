a60a291972f1d1ddf0f90873d7319e4dfe4399bd
ginalzsolt <ginalzsolt@turr.hu>
2023-02-06 09:09:51 +0100

    Error alert rework

--- frontend/src/components/subcomponents/ErrorAlert.svelte
+++ frontend/src/components/subcomponents/ErrorAlert.svelte
@@ -1,5 +1,8 @@
-<div class={visible ? "alert alert-danger d-flex flex-row justify-content-between" : "d-none"} role="alert" id={Error.id}>
-    <div><i class="bi bi-exclamation-triangle-fill"></i>{Error.text} <i class="bi bi-exclamation-triangle-fill"></i></div>
+<div 
+    class="{visible ? "alert d-flex flex-row justify-content-between" : "d-none"}{Error.error?"alert-danger":"alert-success"}" 
+    role="alert" id={Error.id}
+>
+    <div>{Error.text}</div>
     <button type="button" class="btn-close" on:click={()=>{hideError()}}></button>
 </div> 
 
