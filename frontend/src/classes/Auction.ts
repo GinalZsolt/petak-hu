ec30ae4f31e924fb0b0f1e32b2ae167a1b3e05bf
zickbalazs <zickbalazs@turr.hu>
2023-02-06 09:12:21 +0100

    OOP fájlok elkészítve: Aukció, Felhasználó, Zseton

--- frontend/src/classes/Auction.ts
+++ frontend/src/classes/Auction.ts
@@ -1,3 +1,25 @@
 export class Auction{
-    
+    public ID:number;
+    public price:number;
+    public title:string;
+    public minBid:number;
+    public description:string;
+    public expiration:Date;
+    public bidders:Array<Bidder>;
+    constructor (data:any){
+        this.ID = data.ID;
+        this.price=data.price;
+        this.title=data.title;
+        this.minBid=data.minBid;
+        this.description=data.description;
+        this.expiration=data.expiration;
+    }
+}
+
+
+
+interface Bidder{
+    userName:string;
+    amount:number;
+    date:Date;
 }
