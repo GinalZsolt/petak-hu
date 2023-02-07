f02a0d2f60ea24cfcf5a3da3322011b13cfd7bbf
ginalzsolt <ginalzsolt@turr.hu>
2023-02-07 10:56:35 +0100

    Error class selectors fixed

--- frontend/src/components/subcomponents/ErrorAlert.svelte
+++ frontend/src/components/subcomponents/ErrorAlert.svelte
@@ -1,20 +1,23 @@
 <div 
-    class="{visible ? "alert d-flex flex-row justify-content-between" : "d-none"}{Error.error?"alert-danger":"alert-success"}" 
-    role="alert" id={Error.id}
->
+class={visible ? "alert d-flex flex-row justify-content-between" : "d-none"}
+class:alert-danger={danger}
+class:alert-success={success}
+role="alert" id={Error.id}>
     <div>{Error.text}</div>
     <button type="button" class="btn-close" on:click={()=>{hideError()}}></button>
 </div> 
 
 
 <script lang="ts">
+    export let Error:any={}
+    let visible:boolean = false
     function hideError(){
          visible = false;
     }
     export function showError(){
         visible = true;
     }
-    let visible = false;
-    export let Error:any={}
+    let danger:boolean=Error.error
+    let success:boolean=!Error.error
     
 </script>
