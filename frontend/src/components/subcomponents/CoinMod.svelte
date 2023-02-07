74209d5044601b61b533237c418ad9b1a9513e27
ginalzsolt <ginalzsolt@turr.hu>
2023-02-07 12:21:11 +0100

    CoinModal scrollable beállítva

--- frontend/src/components/subcomponents/CoinMod.svelte
+++ frontend/src/components/subcomponents/CoinMod.svelte
@@ -53,7 +53,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="CoinMod" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="staticBackdropLabel">Érme módosítása</h1>
