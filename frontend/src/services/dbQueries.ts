242089f4c088dd773226e82eb079f184843d11f8
zickbalazs <zickbalazs@turr.hu>
2023-02-17 09:03:52 +0100

    Exportálás hozzáadva az axios lekérésekhez

--- frontend/src/services/dbQueries.ts
+++ frontend/src/services/dbQueries.ts
@@ -3,14 +3,13 @@ let backendUrl = 'http://localhost:8080/api';
 
 async function Get(token:string, table:string, field?:string, value?:string){
     let querystring = `/${table}${field ? `/${field}`:''}${value ? `/${value}` : ''}'}`;
-    return axios.get(querystring, {
+    return await axios.get(querystring, {
         headers:{
             'Authorization': 'JWT '+token
         }
     }).then(res=>res.data).catch(err=>err.response);
 }
 function Post(token:string){
-
 }
 function Patch(token:string){
 
@@ -18,3 +17,7 @@ function Patch(token:string){
 function Delete(token:string){
 
 }
+
+
+
+export {Get, Post, Patch, Delete};
