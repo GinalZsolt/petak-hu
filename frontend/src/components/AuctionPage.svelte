b3ef477bbbb02937708f01db8bbb1c828434652a
zickbalazs <zickbalazs@turr.hu>
2023-02-21 09:36:24 +0100

    Aukción modálablak megnyitása az érmére kattintva
    Aukción képek megjelenítése

--- frontend/src/components/AuctionPage.svelte
+++ frontend/src/components/AuctionPage.svelte
@@ -9,10 +9,14 @@
   } from "../services/dbAuction";
   import { Token, userPerms } from "../stores";
   import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+  import type { Coin } from "../interfaces/Coin";
+    import { GetCoin } from "../services/dbCoin";
+    import CoinModal from "./subcomponents/coinModal.svelte";
   
   export let ID: number;
   
-  
+  let coin:Coin;
   let auction: Auction;
   let bidders: Bidder[];
   let originalPrice: number;
@@ -20,9 +24,11 @@
 
 
   onMount(async () => {
-    bidders = await GetBidders($Token.token, ID);
+    bidders = (await GetBidders($Token.token, ID)).sort((a,b)=>new Date(a.date)<new Date(b.date));
     auction = await GetAuctionData($Token.token, ID);
     originalPrice = await (await GetAuctionData($Token.token, ID)).price;
+    coin = (await GetCoin(auction.coinID, $Token.token));
+    console.log(await GetCoin(auction.coinID, $Token.token));
     console.log(originalPrice+auction.minBid);
   });
 
@@ -31,7 +37,7 @@
   socket.on("newPrice", async (data) => {
     auction.price = data;
     originalPrice = data;
-    bidders = await GetBidders($Token.token, ID);
+    bidders = (await GetBidders($Token.token, ID)).sort((a,b)=>new Date(a.date)<new Date(b.date));
   });
 
   function Bid() {
@@ -61,35 +67,53 @@
         <h2 class="mb-0">{auction.userID} - {auction.title}</h2>
       </aside>
       <div class="row mx-auto">
-        <div class="ps-md-0 col-md-6 col-12 mb-md-0 mb-3">
+        {#if coin}
+        <CoinModal coin={coin} />
+        <div class="ps-md-0 col-md-6 col-12 mb-md-0 mb-3" data-bs-target='#coin_' data-bs-toggle="modal">
           <div class="w-100 h-100" id="AuctionCoin">
-            <!--<img class="img-fluid h-100" src={auction} alt="" />
-          <p class="w-100 px-2" id="title">{auction.coin.name}</p>-->
+            <img class="img-fluid h-100" src={'http://localhost:8080/img/'+coin.headfile} alt="" />
+          <p class="w-100 px-2" id="title">{auction.title}</p>
           </div>
         </div>
+        {:else}
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        {/if}
         <div class="col-md-6 col-12 pe-md-0">
           <h3>Licitálás</h3>
           <div class="d-flex flex-row mb-3">
             <div class="input-group me-lg-3">
               <input
                 type="number"
-                min={auction.price + auction.minBid}
+                min={originalPrice}
+                step={auction.minBid}
+                
                 bind:value={auction.price}
+
                 name="bidAmount"
                 class="form-control border-dark"
               />
               <label for="bidAmount" class="input-group-text border-dark">Ft</label>
             </div>
-            <button class="btn border-dark" on:click={Bid}>Licitálás</button>
+            <button class="btn border-dark ms-2" on:click={Bid}>Licitálás</button>
           </div>
           <p class="border-bottom border-dark pb-3">
             Licitlépcső: {auction.minBid} Ft
           </p>
           <h3>Legutóbbi licitek</h3>
           <div id="Bidders">
-            {#each bidders as bidder}
-              <p>{bidder.username} - {bidder.price} {bidder.date}</p>
-            {/each}
+            {#if bidders}
+              {#each bidders as bidder}
+                <p transition:fade>{bidder.username} - {bidder.price} Ft | {new Intl.DateTimeFormat('hu-HU', {
+                  dateStyle:'long',
+                }).format(new Date(bidder.date))} - {bidder.date.split('T')[1].split('.')[0]}</p>
+              {/each}
+              {:else}
+                <div class="spinner-border" role="status">
+                  <span class="visually-hidden">Loading...</span>
+                </div>
+            {/if}
           </div>
         </div>
       </div>
@@ -99,7 +123,19 @@
         {#each auction.description.split("\n") as line}
           <p>{line}</p>
         {/each}
+        <hr>
+        {#if coin}
+        <h3>Az érméről:</h3>
+        {#each coin.description.split("\n") as line}
+          <p>{line}</p>
+        {/each}
+        {/if}
       </div>
+    </div>
+    
+    {:else}
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
     </div>
   {/if}
 </main>
