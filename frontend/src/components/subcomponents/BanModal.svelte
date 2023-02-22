7bb0beea811fb0b96150e1739110b4dba388decf
ginalzsolt <ginalzsolt@turr.hu>
2023-02-22 11:22:12 +0100

    BanModal,Profile automatikus feltöltés

--- frontend/src/components/subcomponents/BanModal.svelte
+++ frontend/src/components/subcomponents/BanModal.svelte
@@ -1,37 +1,50 @@
 <script lang="ts">
 
 export let User:any={}
+import ErrorAlert from "./ErrorAlert.svelte";
 
-function Ban(){}
+let err1
+function Ban(){
+  if (User.datum==undefined){
+    err1.showError()
+  }
+}
 
 </script>
 
+<style lang="sass">
+  .btn
+        background-color: #ea9e60
+</style>
+
 <!-- Modal -->
+
 <div class="modal fade" id="Ban" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="staticBackdropLabel">{User.username} felhasználó kitiltása</h1>
-          <input type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-          </div>
-          <div class="modal-body">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">{User.name} felhasználó kitiltása</h1>
+        <input type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+      </div>
+      <div class="modal-body">
+        <ErrorAlert bind:this={err1} Error={{id:"nodate",text:"Nem adtál meg dátumot!",error:true}}/>
             <form action="">
               <div class="mb-3">
-                <label for="username" class="form-label">Email cím</label>
-                <input type="text" bind:value={User.username} class="form-control" id="username" name="username" >
+                <label for="username" class="form-label">Felhasználó név</label>
+                <input type="text" bind:value={User.name} disabled class="form-control" id="username" name="username" >
               </div> 
               <div class="mb-3">
                 <label for="email" class="form-label">Email cím</label>
-                <input type="email" bind:value={User.email} class="form-control" id="email" name="email" >
+                <input type="email" bind:value={User.email} disabled class="form-control" id="email" name="email" >
               </div>
               <div class="mb-3">
                 <label for="datum" class="form-label">Felfüggesztés lejáratának dátuma</label>
-                <input type="datetime-local" bind:value={User.datum} class="form-control" id="datum" name="datum" >
+                <input type="date" bind:value={User.datum} class="form-control" id="datum" name="datum" >
               </div>
             </form>
           </div>
           <div class="modal-footer">
-          <button type="button" on:click={Ban} class="btn">Felfüggesztés</button>
+          <button type="button" on:click={()=>{Ban()}} class="btn">Felfüggesztés</button>
         </div>
       </div>
     </div>
