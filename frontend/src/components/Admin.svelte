c4c827e2da2f4c9ea2ed38e1dff6d8bd71b3e3de
ginalzsolt <ginalzsolt@turr.hu>
2023-02-24 11:22:27 +0100

    admincard

---
+++ frontend/src/components/Admin.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import AdminboardCard from "./subcomponents/DashboardCard.svelte";
+
+
+
+</script>
+<style lang="sass">
+
+</style>
+
+<main>
+    <h2>Adminisztráció</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <AdminboardCard  />
+            </div>
+            <div class="col-4">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+
+            </div>
+            <div class="col-4">
+
+            </div>
+        </div>
+    </div>
+</main>
---
+++ frontend/src/components/subcomponents/AdminboardCard.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let CardData: any;
+</script>
+<div
+    class="dashboard-card m-3"
+    style:--color1={CardData.fromColor}
+    style:--color2={CardData.toColor}
+    data-bs-toggle="modal" data-bs-target="#{CardData.modalName}"
+>
+    <div class="h-100 d-flex justify-content-between p-2">
+        <h2 class="fs-1 align-self-start">{CardData.title}</h2>
+        <i class={`display-1 bi bi-${CardData.icon} align-self-end`} />
+    </div>
+</div>
+<style lang="sass">
+    @function gradient($fromColor, $toColor)
+        @return linear-gradient(172deg, $fromColor, $toColor)
+    a
+        color: white
+        text-decoration: none
+    .dashboard-card
+        background: gradient(var(--color1), var(--color2))
+        border-radius: 0.5rem
+        height: 200px
+        .bi
+            color: #0000004f
+</style>
