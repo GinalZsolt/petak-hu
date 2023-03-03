dc7511974e8445567702843740019d4469b6684c
zickbalazs <zickbalazs@turr.hu>
2023-03-03 12:29:24 +0100

    Alap statisztika lekérdezés

--- frontend/src/components/Admin.svelte
+++ frontend/src/components/Admin.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
-    import AdminboardCard from "./subcomponents/AdminboardCard.svelte";
-
+    import { onMount } from "svelte";
+    import {Token} from '../stores';
+    import type {ModalData} from '../interfaces/Admin';
+    import { getCoinAmountStats, getCoinWorthStats } from "../services/dbStatistics";
+    import AdminboardCard from "./subcomponents/Admin/AdminboardCard.svelte";
+    import AdminStatisticsModal from "./subcomponents/Admin/adminStatisticsModal.svelte";
+    let statisticsModalData:ModalData;
+    function DetermineStatistic(event){
+        switch(event.detail.title){
+            case "Látogatók száma": {
+                statisticsModalData = {
+                    promise: getCoinAmountStats($Token.token),
+                    title:'Coinis'
+                }
+            }
+        }
+    }
 
 
 </script>
@@ -13,19 +28,20 @@
     <div class="container">
         <div class="row">
             <div class="col-6">
-                <AdminboardCard CardData={{modalName:"latogatok",title:"Látogatók száma",icon:"person",fromColor:"#ea9e60",toColor:"#eb6a00"}} />
+                <AdminboardCard on:clicked={DetermineStatistic} CardData={{title:"Látogatók száma",icon:"person",fromColor:"#ea9e60",toColor:"#eb6a00"}} />
             </div>
             <div class="col-6">
-                <AdminboardCard CardData={{modalName:"ermek",title:"Érmék",icon:"coin",fromColor:"#ea9e60",toColor:"#eb6a00"}} />
+                <AdminboardCard CardData={{title:"Érmék",icon:"coin",fromColor:"#ea9e60",toColor:"#eb6a00"}} />
             </div>
         </div>
         <div class="row">
             <div class="col-6">
-                <AdminboardCard CardData={{modalName:"tiltott",title:"Kitiltott Felhasználók",icon:"person-exclamation",fromColor:"#ea9e60",toColor:"#eb6a00"}} />
+                <AdminboardCard CardData={{title:"Kitiltott Felhasználók",icon:"person-exclamation",fromColor:"#ea9e60",toColor:"#eb6a00"}} />
             </div>
             <div class="col-6">
-                <AdminboardCard CardData={{modalName:"aukciok",title:"Aukciók",icon:"database",fromColor:"#ea9e60",toColor:"#eb6a00"}} />
+                <AdminboardCard CardData={{title:"Aukciók",icon:"database",fromColor:"#ea9e60",toColor:"#eb6a00"}} />
             </div>
         </div>
     </div>
+    <AdminStatisticsModal Data={statisticsModalData} />
 </main>
---
+++ frontend/src/components/subcomponents/Admin/adminStatisticsModal.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import type { ModalData } from "../../../interfaces/Admin";
+    import type { CoinAmountStatistic, CoinWorthStatistic } from "../../../services/dbStatistics";
+
+    
+    export let Data:ModalData
+    
+</script>
+<style lang="ts"></style>
+<div class="modal modal-lg" tabindex="-1" id="statisticsModal">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header bg-primary bg-gradient text-light">
+          <h5 class="modal-title">Statisztika</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          {#if Data}
+            {#await Data.promise}
+            <div class="spinner"></div>
+            {:then Statistics}
+            <p>Modal body text goes here.</p>
+            {/await}
+          {/if}
+        </div>
+      </div>
+    </div>
+</div>
+  
