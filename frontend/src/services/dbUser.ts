261aed0ed366dd7fed2aca1d6627e594710e31db
ginalzsolt <ginalzsolt@turr.hu>
2023-03-06 14:02:18 +0100

    Ban service

--- frontend/src/services/dbUser.ts
+++ frontend/src/services/dbUser.ts
@@ -2,9 +2,44 @@ import type { User } from "../classes/User";
 import axios from 'axios';
 import { Get } from "./dbQueries";
 import {Token} from '../stores';
+
+interface Moderations{
+    ID:number,
+    userID:number,
+    date:string,
+    banTime:string
+}
+
+interface DBUser{
+    ID:number,
+    name:string,
+    email:string
+}
+
+interface Banned{
+    ID:number,
+    userID:number,
+    date:string,
+    email:string,
+    name:string,
+}
+
 async function GetUserData(id:number, token:string):Promise<User>{
     return await Get(token, 'users', 'ID', id);
 }
 
-
+async function GetBanned(token:string):Promise<Banned[]> {
+    let moderations:Promise<Moderations[]> = axios.get('http://localhost:8080/api/moderations',{headers:{"Authorization":"JWT "+token}})
+    let users:Promise<DBUser[]> = axios.get('http://localhost:8080/api/users',{headers:{"Authorization":"JWT "+token}})
+    return Promise.all([moderations,users]).then(res=>
+        res[0].map( e=>
+            {
+                return {
+                    ...e,
+                    ...res[1].find(x=>x.ID==e.userID)
+                }
+            }
+            )as Banned[]
+    )
+}
 export {GetUserData};
