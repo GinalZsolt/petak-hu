44959ddc4bfcd88b597838a133bf11e9ecfa0f13
zickbalazs <balazs.zick2@gmail.com>
2023-03-12 17:58:47 +0100

    Profil Revizió kinézete kész

--- frontend/src/services/dbUser.ts
+++ frontend/src/services/dbUser.ts
@@ -2,6 +2,9 @@ import type { User } from "../classes/User";
 import axios from 'axios';
 import { Get } from "./dbQueries";
 import {Token} from '../stores';
+import type { Profile } from "../interfaces/Profile";
+import type { Auction } from "../interfaces/Auction";
+import type { Coin } from "../interfaces/Coin";
 
 interface Moderations{
     ID:number,
@@ -28,7 +31,44 @@ export interface Banned{
 async function GetUserData(id:number, token:string):Promise<User>{
     return await Get(token, 'users', 'ID', id);
 }
+async function GetUserProfile(id:number, token:string):Promise<Profile>{
+    let user = await Get(token, 'users', 'ID', id) as Promise<User[]>;
+    let auctions = await Get(token, 'auctions', 'userID', id) as Promise<Auction[]>;
+    let coins = await Get(token, 'coins', 'userID', id) as Promise<Coin[]>;
 
+    return Promise.all([user, auctions, coins]).then(res=>{
+        return {
+            user: {
+                ...res[0][0]
+            },
+            auctions: res[1].filter(g=>g.userID==res[0][0].ID).map(e=>{
+                return {
+                    ID: e.ID,
+                    userID: e.userID,
+                    coinID: e.coinID,
+                    description: e.description,
+                    expiration: e.expiration,
+                    minBid: e.minBid,
+                    notified: e.notified,
+                    price: e.price,
+                    title: e.title,
+                } as Auction
+            }),
+            coins: res[2].filter(g=>g.userID==id).map(e=>{
+                return {
+                    ID: e.ID,
+                    description: e.description,
+                    headfile: e.headfile,
+                    tailfile: e.tailfile,
+                    name: e.name,
+                    uploadDate: e.uploadDate,
+                    userID: e.userID,
+                    worth: e.worth
+                } as Coin
+            })
+        } as Profile
+    })
+}
 async function GetBanned(token:string):Promise<Banned[]> {
     let moderations:Promise<Moderations[]> = axios.get('http://localhost:8080/api/moderations',{headers:{"Authorization":"JWT "+token}})
     let users:Promise<DBUser[]> = axios.get('http://localhost:8080/api/users',{headers:{"Authorization":"JWT "+token}})
@@ -48,4 +88,4 @@ async function GetBanned(token:string):Promise<Banned[]> {
             )as Banned[]
     )
 }
-export {GetUserData,GetBanned};
+export { GetUserData, GetBanned, GetUserProfile };
