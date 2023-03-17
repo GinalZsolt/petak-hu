601f98da9e1220f97898a5fa0ded9342e192d06c
ginalzsolt <ginalzsolt@turr.hu>
2023-03-17 12:52:27 +0100

    nvabar brand link hozzáadva

--- frontend/src/components/NavBar.svelte
+++ frontend/src/components/NavBar.svelte
@@ -17,7 +17,7 @@
     {#if $userPerms}
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Peták.hu</a>
+          <a class="navbar-brand" href="/">Peták.hu</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
