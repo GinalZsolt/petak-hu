6f89fbdcdb92cd3f64ae845bf7ac613db287aaa2
ginalzsolt <ginalzsolt@turr.hu>
2023-03-24 12:17:42 +0100

    Promo szöveg kiegészítés

--- frontend/src/components/Promo.svelte
+++ frontend/src/components/Promo.svelte
@@ -11,11 +11,19 @@
     <div>
         <div class="row">
             <img src="" alt="">
-            <p>A Peták.hu egy gyújtői és aukciós oldal, melyet elsősorban a Magyar Éremgyűjtők Egyesületének tagjai számára fejlesztettünk ki. Ide minden gyűjtő feltöltheti érméit, kereshet köztük, elküldheti katalógusát ismerőseinek illetve a már meglévő érméit aukcióra bocsáthatja. Ezek mellett az oldal rendelkezik egy fórum felülettel is, ahol a tagok egymástól segítséget kérhetnek egyes érmék beazonosításában, értékenek megállapításában vagy esetleg más-más érmékkel kapcsolatos témákról is tudnak társalgást folytatni. </p>
+            <p>A Peták.hu egy gyújtői és aukciós oldal, melyet elsősorban a Magyar Éremgyűjtők Egyesületének tagjai számára fejlesztettünk ki. Ide minden gyűjtő feltöltheti érméit, kereshet köztük, elküldheti katalógusát ismerőseinek illetve a már meglévő érméit aukcióra bocsáthatja.  </p>
+        </div>
+        <div class="row">
+            <p>Az oldal könnyen kezelhető, akár telefonról is, ennek köszönhetően bármikor hozzáférhet katalógusához. Ebben segítséget nyújt az oldal tetején elhelyezett kereső sáv, melyben az érme neve és címkéi alapján is kereshet. Innen a katalógusból tudja feltöltött érméit módosítani, aukcióra bocsátani illetve törölni.</p>
+            <img src="" alt="">
+        </div>
+        <div class="row">
+            <p>Ezek mellett az oldal rendelkezik egy fórum felülettel is, ahol a tagok egymástól segítséget kérhetnek egyes érmék beazonosításában, értékenek megállapításában vagy esetleg más-más érmékkel kapcsolatos témákról is tudnak társalgást folytatni.</p>
+            <img src="" alt="">
         </div>
         <div class="row">
             <img src="" alt="">
-            <p>Az oldal könnyen kezelhető, akár telefonról is, ennek köszönhetően bármikor hozzáférhet katalógusához. Ebben segítséget nyújt az oldal tetején elhelyezett kereső sáv, melyben az érme neve és címkéi alapján is kereshet. Innen a katalógusból tudja feltöltött érméit módosítani, aukcióra bocsátani illetve törölni.</p>
+            <p>Ha a felhasználók szeretnék eladni érméiket, ezt aukciók segítségével tehetik meg, amiben az oldalunk segít. Az aukciós fülön láthatják a legfrissebb aukciókat, vagy a kereső sáv segítségevel találhatják meg az általuk keresett érmét.</p>
         </div>
     </div>
 </main>
