219b5299fb8d9e63a57d107cb102034d56a5cde8
ginalzsolt <ginalzsolt@turr.hu>
2023-03-24 12:07:58 +0100

    Promo szöveg kész

---
+++ frontend/src/components/Promo.svelte
@@ -0,0 +1,21 @@
+<style lang="sass">
+    #heading
+        height: 50vh
+        background: rgb(235,106,0)
+        background: linear-gradient(45deg, rgba(235,106,0,1) 0%, rgba(216,140,80,1) 100%)
+</style>
+<main>
+    <div id="heading">
+        <h1>Peták.hu</h1>
+    </div>
+    <div>
+        <div class="row">
+            <img src="" alt="">
+            <p>A Peták.hu egy gyújtői és aukciós oldal, melyet elsősorban a Magyar Éremgyűjtők Egyesületének tagjai számára fejlesztettünk ki. Ide minden gyűjtő feltöltheti érméit, kereshet köztük, elküldheti katalógusát ismerőseinek illetve a már meglévő érméit aukcióra bocsáthatja. Ezek mellett az oldal rendelkezik egy fórum felülettel is, ahol a tagok egymástól segítséget kérhetnek egyes érmék beazonosításában, értékenek megállapításában vagy esetleg más-más érmékkel kapcsolatos témákról is tudnak társalgást folytatni. </p>
+        </div>
+        <div class="row">
+            <img src="" alt="">
+            <p>Az oldal könnyen kezelhető, akár telefonról is, ennek köszönhetően bármikor hozzáférhet katalógusához. Ebben segítséget nyújt az oldal tetején elhelyezett kereső sáv, melyben az érme neve és címkéi alapján is kereshet. Innen a katalógusból tudja feltöltött érméit módosítani, aukcióra bocsátani illetve törölni.</p>
+        </div>
+    </div>
+</main>
