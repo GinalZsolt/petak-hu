128f678b831aa8775c1df994cf34c2d56a7e8825
ginalzsolt <ginalzsolt@turr.hu>
2023-03-31 11:48:26 +0200

    promo

--- frontend/src/components/Promo.svelte
+++ frontend/src/components/Promo.svelte
@@ -10,20 +10,20 @@
     </div>
     <div>
         <div class="row">
-            <img src="" alt="">
-            <p>A Peták.hu egy gyújtői és aukciós oldal, melyet elsősorban a Magyar Éremgyűjtők Egyesületének tagjai számára fejlesztettünk ki. Ide minden gyűjtő feltöltheti érméit, kereshet köztük, elküldheti katalógusát ismerőseinek illetve a már meglévő érméit aukcióra bocsáthatja.  </p>
+            <img class="col-12 col-md-4 img-thumbnail" src="MÉE.jpg" alt="">
+            <p class="my-auto col-12 col-md-8">A Peták.hu egy gyújtői és aukciós oldal, melyet elsősorban a Magyar Éremgyűjtők Egyesületének tagjai számára fejlesztettünk ki. Ide minden gyűjtő feltöltheti érméit, kereshet köztük, elküldheti katalógusát ismerőseinek illetve a már meglévő érméit aukcióra bocsáthatja.  </p>
         </div>
         <div class="row">
-            <p>Az oldal könnyen kezelhető, akár telefonról is, ennek köszönhetően bármikor hozzáférhet katalógusához. Ebben segítséget nyújt az oldal tetején elhelyezett kereső sáv, melyben az érme neve és címkéi alapján is kereshet. Innen a katalógusból tudja feltöltött érméit módosítani, aukcióra bocsátani illetve törölni.</p>
-            <img src="" alt="">
+            <p class="my-auto col-12 col-md-8">Az oldal könnyen kezelhető, akár telefonról is, ennek köszönhetően bármikor hozzáférhet katalógusához. Ebben segítséget nyújt az oldal tetején elhelyezett kereső sáv, melyben az érme neve és címkéi alapján is kereshet. Innen a katalógusból tudja feltöltött érméit módosítani, aukcióra bocsátani illetve törölni.</p>
+            <img class="col-12 col-md-4 img-thumbnail" src="/pic1.png" alt="Katalógus">
         </div>
         <div class="row">
-            <p>Ezek mellett az oldal rendelkezik egy fórum felülettel is, ahol a tagok egymástól segítséget kérhetnek egyes érmék beazonosításában, értékenek megállapításában vagy esetleg más-más érmékkel kapcsolatos témákról is tudnak társalgást folytatni.</p>
-            <img src="" alt="">
+            <img class="col-12 col-md-4 img-thumbnail" src="" alt="">
+            <p class="my-auto col-12 col-md-8">Ezek mellett az oldal rendelkezik egy fórum felülettel is, ahol a tagok egymástól segítséget kérhetnek egyes érmék beazonosításában, értékenek megállapításában vagy esetleg más-más érmékkel kapcsolatos témákról is tudnak társalgást folytatni.</p>
         </div>
         <div class="row">
-            <img src="" alt="">
-            <p>Ha a felhasználók szeretnék eladni érméiket, ezt aukciók segítségével tehetik meg, amiben az oldalunk segít. Az aukciós fülön láthatják a legfrissebb aukciókat, vagy a kereső sáv segítségevel találhatják meg az általuk keresett érmét.</p>
+            <p class="my-auto col-12 col-md-8">Ha a felhasználók szeretnék eladni érméiket, ezt aukciók segítségével tehetik meg, amiben az oldalunk segít. Az aukciós fülön láthatják a legfrissebb aukciókat, vagy a kereső sáv segítségevel találhatják meg az általuk keresett érmét.</p>
+            <img class="col-12 col-md-4 img-thumbnail" src="" alt="">
         </div>
     </div>
 </main>
